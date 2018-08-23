a8c56362f8f8aa4544fc9a6b05e2544e279f1602
Hadi Ahmadi <MacPersia@gmail.com>
2018-08-24 01:18:29 +0800

    Minor update to launch intros

--- src/main/java/be/planty/skills/prototyping/PrototypingStreamHandler.java
+++ src/main/java/be/planty/skills/prototyping/PrototypingStreamHandler.java
@@ -1,19 +1,33 @@
 package be.planty.skills.prototyping;
 
 import be.planty.skills.assistant.AssistantStreamHandler;
+import be.planty.skills.assistant.handlers.*;
+import be.planty.skills.assistant.handlers.interceptors.MyRequestInterceptor;
+import be.planty.skills.assistant.handlers.interceptors.MyResponseInterceptor;
+import be.planty.skills.prototyping.handlers.LaunchRequestHandler;
 import be.planty.skills.prototyping.handlers.NewWebAppIntentHandler;
-import com.amazon.ask.Skill;
+import com.amazon.ask.Skills;
+import com.amazon.ask.builder.SkillBuilder;
 
 public class PrototypingStreamHandler extends AssistantStreamHandler {
 
-    private static Skill getSkill() {
-        return getSkillBuilder()
-                .addRequestHandler(
-                        new NewWebAppIntentHandler()
-                ).build();
+    protected static SkillBuilder getSkillBuilder() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new HelpIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new HelloWorldIntentHandler(),
+                        new EmailAddressIntentHandler(),
+                        new FallbackIntentHandler(),
+                        new CancelandStopIntentHandler(),
+                        new SessionEndedRequestHandler(),
+                        new NewWebAppIntentHandler())
+                .addRequestInterceptor(new MyRequestInterceptor())
+                .addResponseInterceptor(new MyResponseInterceptor())
+                .addExceptionHandler(new MyExecptionHandler());
     }
 
     public PrototypingStreamHandler() {
-        super(getSkill());
+        super(getSkillBuilder().build());
     }
  }

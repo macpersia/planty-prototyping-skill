333cedeeff658e12e97c4e883a0b6a3a8b687d4c
Hadi Ahmadi <MacPersia@gmail.com>
2018-11-28 10:10:19 +0800

    Minor fix

--- src/main/java/be/planty/skills/prototyping/handlers/agent/AgentSessionHandler.java
+++ src/main/java/be/planty/skills/prototyping/handlers/agent/AgentSessionHandler.java
@@ -30,7 +30,7 @@ public class AgentSessionHandler extends be.planty.skills.assistant.handlers.age
     public void handleFrame(StompHeaders headers, Object payload) {
 
         if (payload instanceof ActionResponse
-                || this.input.matches(intentName(INTENT_NEW_WEB_APP))) {
+                && this.input.matches(intentName(INTENT_NEW_WEB_APP))) {
             try {
                 final String prettyJson = objectWriter.writeValueAsString(payload);
                 logger.info("Received action response: " + prettyJson);
